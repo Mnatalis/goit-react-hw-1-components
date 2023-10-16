e8fe4bc814fb5eda3e6d3cb4274b6e14cd80b80d
Nataliss <oksklient@gmail.com>
2023-10-16 10:40:09 +0100

    Update bg color

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,11 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   const allLabels = stats.map(item => {
     return (
-      <li key={item.id} className={css.item}>
+      <li
+        key={item.id}
+        className={css.item}
+        style={{ backgroundColor: getRandomHexColor() }}
+      >
         <p className={css.label}>{item.label}</p>
         <span className={css.percentage}>{item.percentage}%</span>
       </li>
@@ -17,3 +21,9 @@ export const Statistics = ({ title, stats }) => {
     </div>
   );
 };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
